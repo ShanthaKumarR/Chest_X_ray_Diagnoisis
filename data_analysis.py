37b15b896b0ef11836079a2afd3a0ac8f38564e7
ShanthaKumarR <shanthakumar.rajasekar@gmail.com>
2021-03-24 17:22:28 +0100

    data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -3,32 +3,54 @@ import pandas as pd
 import seaborn as sn 
 import matplotlib.pyplot as plt
 import os
+
 train_A = pd.read_csv('train_A.csv')
 
-
 class Data_analysis:
-    def __init__(self, data):
-        self.data = data    
+    def __init__(self, data, image_dir):
+        self.data = data  
+        self.image_dir = image_dir  
     def data_insight(self):
         print('The first five records are: \n ', self.data.head(5))
         print('The colum attributes(labels) are: \n', self.data.columns)
         print('The null value check: \n', self.data.info())
         print('No of unique patient Id is: \n', len(self.data['PatientId'].unique()))
         print(self.data.sum())
-        return self.data.drop(['Image', 'PatientId'], 1, inplace=True)
-    def image_visualization(self, image_dir):
+        return self.data.drop(['Image', 'PatientId'], 1, inplace=False)
+    def image_visualization(self):
         images = self.data['Image'].values
-        images = [np.random.choice(images) for i in range(9)]
+        images = [np.random.choice(images) for i in range(9)]        
         plt.figure(figsize=(20,10))
         for i in range(9):
             plt.subplot(3, 3, i+1)
-            img = plt.imread(image_dir+'/'+images[i])
+            img = plt.imread(self.image_dir+'/'+images[i])
             plt.imshow(img, cmap= 'gray')
             plt.axis('off')
         plt.show()
+    def pixel_distrubution(self):
+        images = self.data['Image'].values
+        images = np.random.choice(images)
+        original_example = plt.imread(self.image_dir+'/'+images)
+        print('The dimention of the image is:', original_example.shape)
+        print('pixel mean: ', np.mean(original_example))
+        print('pixel Standard Deviation: ', np.std(original_example))
+       # plt.figure(figsize=(12,4))
+        #plt.subplot(121)
+        #plt.imshow(original_example, cmap= 'gray')
+        #plt.show()        
+        plt.figure(figsize=(12,4))
+        plt.subplot(121)
+        sn.distplot(original_example, kde=True)
+        plt.xlabel('Pixel Intensity', fontsize=14)
+        plt.ylabel('# Pixels in Image', fontsize=14)
+        plt.subplot(122)
+        sn.distplot(original_example, kde=False)
+        plt.xlabel('Pixel Intensity', fontsize=14)
+        plt.ylabel('# Pixels in Image', fontsize=14)
 
-        #print(images)
 
-X = Data_analysis(train_A)
-#y=X.data_insight()
-X.image_visualization(image_dir = 'D:/material_science/rwa/AI-For-Medicine-Specialization-master/AI for Medical Diagnosis/Week 1/nih/images-small')
+
+X = Data_analysis(train_A, image_dir = 'D:/material_science/rwa/AI-For-Medicine-Specialization-master/AI for Medical Diagnosis/Week 1/nih/images-small')
+y=X.data_insight()
+X.pixel_distrubution()
+X.image_visualization()
---
+++ image_preprocessing.py
@@ -0,0 +1,57 @@
+from keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
+import seaborn as sn 
+import matplotlib.pyplot as plt
+import numpy as np 
+
+
+
+train_A = pd.read_csv('train_A.csv')
+image_dir = 'D:/material_science/rwa/AI-For-Medicine-Specialization-master/AI for Medical Diagnosis/Week 1/nih/images-small'
+
+
+images = train_A['Image'].values
+images = np.random.choice(images)
+original_example = plt.imread(image_dir+'/'+images)
+
+#Normalize images : new mean of the data will be zero, and the standard deviation of the data will be 1.
+#In other words each pixel value in the image with a new value calculated by subtracting the mean and dividing by the standard deviation.
+image_generator = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization= True)
+generator = image_generator.flow_from_dataframe(
+        dataframe=train_A,
+        directory=image_dir,
+        x_col="Image", 
+        y_col= ['Mass'], 
+        class_mode="raw", 
+        batch_size= 1, 
+        shuffle=False,
+        target_size=(320,320) 
+)
+
+sn.set_style("white")
+generated_image, label = generator.__getitem__(0)
+plt.imshow(generated_image[0], cmap='gray')
+plt.colorbar()
+plt.title('Raw Chest X Ray Image')
+print(f"The dimensions of the image are {generated_image.shape[1]} pixels width and {generated_image.shape[2]} pixels height")
+print(f"The maximum pixel value is {generated_image.max():.4f} and the minimum is {generated_image.min():.4f}")
+print(f"The mean value of the pixels is {generated_image.mean():.4f} and the standard deviation is {generated_image.std():.4f}")
+plt.show()
+
+sn.set()
+plt.figure(figsize=(10, 7))
+sn.distplot(original_example.ravel(), 
+             label=f'Original Image: mean {np.mean(original_example):.4f} - Standard Deviation {np.std(original_example):.4f} \n '
+             f'Min pixel value {np.min(original_example):.4} - Max pixel value {np.max(original_example):.4}',
+             color='blue', 
+             kde=False)
+sn.distplot(generated_image[0].ravel(), 
+             label=f'Generated Image: mean {np.mean(generated_image[0]):.4f} - Standard Deviation {np.std(generated_image[0]):.4f} \n'
+             f'Min pixel value {np.min(generated_image[0]):.4} - Max pixel value {np.max(generated_image[0]):.4}', 
+             color='red', 
+             kde=False)
+plt.legend()
+plt.title('Distribution of Pixel Intensities in the Image')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('# Pixel')
+plt.show()
